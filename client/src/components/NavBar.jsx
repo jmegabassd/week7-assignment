ce247176ef1e09579d08e7e30235577dcb6ca41e
jmegabassd <jmegabassd@gmail.com>
2025-09-26 11:31:14 +0100

    Task: add components

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="nav-bar">
+        <button id="home">Home</button>
+        <button id="create">Create</button>
+        <button id="edit">Edit</button>
+        <button id="delete">Delete</button>
+      </div>
+    </>
+  );
+}
