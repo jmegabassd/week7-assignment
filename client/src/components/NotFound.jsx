a6894e74e2fca3482278381ca1a7b1b765d61f5b
jmegabassd <jmegabassd@gmail.com>
2025-09-27 12:17:47 +0100

    Update NotFound page styling

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,3 +1,4 @@
+import "./NotFound.css";
 import { Link } from "react-router";
 
 export default function NotFound() {
@@ -5,8 +6,11 @@ export default function NotFound() {
     <>
       <div className="not-found">
         <h1>404 Not Found</h1>
-        <p>Where am I, where are you, we are nowhere! lets go home...</p>
-        <Link to={"/"}>Go Home</Link>
+        <br />
+        <p>
+          Where am I, where are you, we are nowhere! lets{" "}
+          <Link to={"/"}>Go Home</Link>...
+        </p>
       </div>
     </>
   );
