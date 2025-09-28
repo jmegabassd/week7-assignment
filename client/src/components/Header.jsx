5820365b1a71d699d33184b2e9b19bf6bb7f01eb
jmegabassd <jmegabassd@gmail.com>
2025-09-28 21:23:01 +0100

    Chore: More styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <header className="header">
-        <h1>Character creation</h1>
+        <h1 className="heading">Character creation</h1>
       </header>
     </>
   );
